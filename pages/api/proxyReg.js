7cd11102851ec5d9bb278b6022b1d9e9dc74a459
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-28 16:45:34 +0300

    qwesad

--- pages/api/proxyReg.js
+++ pages/api/proxyReg.js
@@ -5,9 +5,19 @@ const proxyRequest = async (req, res) => {
     const response = await axios
         .get(`${proxyURL}`)
         .then(({ data }) => {
-            console.log('proxyURL: ', proxyURL ? proxyURL : 'buttfuck')
+            console.log('proxyURL: ', proxyURL ? proxyURL : 'butt');
             return data
-        })
+          })
+          res.setHeader('Access-Control-Allow-Credentials', true)
+          res.setHeader('Access-Control-Allow-Origin', '*')
+          // another common pattern
+          // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+          res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+          res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+            )
+            console.log('res.headers: ', res.headers  ? res.headers  : 'butt origin');
     res.send(response,
         // text: `${req.body}`,
         // duh: proxyURL,
